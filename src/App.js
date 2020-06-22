c1a88b085783412d173eb03067c200274a95dec5
David Fernando <30445601+david-fernando@users.noreply.github.com>
2020-06-22 13:14:01 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Página apenas para testar os serviços da <a className="App-link" href="https://versel.com">Versel</a>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
