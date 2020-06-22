40149427066a26df512910bb27349f144ce02e25
David Fernando <30445601+david-fernando@users.noreply.github.com>
2020-06-22 14:07:57 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,14 +10,6 @@ function App() {
         <p>
           Página apenas para testar os serviços da <a className="App-link" href="https://versel.com">Versel</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
